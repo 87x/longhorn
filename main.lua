bfa9ad21398ee4a7d95188965d8ae6fe20cd55be
yuyuko_S75 <lassi.mantyla@gmail.com>
2023-08-13 06:26:37 +0300

    various changes
    experimental: multiple guilds support
    hack: play all URLs given to it associated with audio MIME type
    deprecated: playlists support

--- main.lua
+++ main.lua
@@ -9,8 +9,11 @@ local client = discordia.Client()
 local connection
 local msg = ''
 local channel
+local guild
 local playingURL = ''
 local playingTrack = 0
+
+connections={}
 
 if not args[2] then
   print("Please specify a token.")
@@ -21,21 +24,45 @@ print(client.voice)
 
 print(args[2])
 
+local function splitstr(inputstr, sep)
+  if not sep then
+    sep = "%s"
+  end
+  local t={}
+  for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+    table.insert(t, str)
+  end
+  return t
+end
+
 local function getStream(url)
 
   local child = spawn('yt-dlp', {
-    args = {"-g",url},
+    args = {"--max-downloads","1","--print","urls","--no-audio-multistreams","--no-video-multistreams","--default-search","ytsearch15:search",url},
     stdio = {nil, true, true}
   })
 
   local stream
+  local streams
   local function readstdout()
     local stdout = child.stdout
     for chunk in stdout.read do
       local mime = parse(chunk, true).query.mime
-      print(mime)
-      if mime and (mime[1] and mime[1]:find('audio')) or (mime[2] and mime[2]:find('audio')) then
-        stream = chunk
+      print("mime",mime)
+      print("chunk",chunk)
+      if mime then
+        if type(mime)=="table" then
+          for i,v in pairs(mime) do
+            print("mime DATA",v)
+            if v:find('audio') then
+              streams=splitstr(chunk)
+              stream=streams[i]
+            end
+          end
+        elseif type(mime)=="string" and mime:find('audio') then
+          streams=splitstr(chunk)
+          stream="multiple" -- HACK
+        end
       end
     end
     return pcall(stdout.handle.close, stdout.handle)
@@ -51,12 +78,12 @@ local function getStream(url)
 
   split(readstdout, readstderr, child.waitExit)
 
-  return stream and stream:gsub('%c', '')
+  return stream and stream:gsub('%c', ''),streams
 end
 
-local function getPlaylistStream(url, number)
+--[[local function getPlaylistStream(url, number)
   local child = spawn('yt-dlp', {
-    args = {'-g', '--playlist-items', number, url},
+    args = {'--print urls', '--playlist-items', number, url},
     stdio = {nil, true, true}
   })
 
@@ -65,8 +92,14 @@ local function getPlaylistStream(url, number)
     local stdout = child.stdout
     for chunk in stdout.read do
       local mime = parse(chunk, true).query.mime
-      if mime and mime:find('audio') then
-        stream = chunk
+      print("mime",mime)
+      if mime then
+        for i,v in pairs(mime) do
+          print("mime DATA",v)
+          if v:find('audio') then
+            stream=chunk
+          end
+        end
       end
     end
     return pcall(stdout.handle.close, stdout.handle)
@@ -83,7 +116,7 @@ local function getPlaylistStream(url, number)
   split(readstdout, readstderr, child.waitExit)
 
   return stream and stream:gsub('%c', '')
-end
+end]] -- deprecated
 
 local function len(tbl)
   local count = 0
@@ -93,7 +126,7 @@ local function len(tbl)
   return count
 end
 
-local streamPlaylist = coroutine.wrap(function(url, beginWith)
+--[[local streamPlaylist = coroutine.wrap(function(url, beginWith)
   local child = spawn('yt-dlp', {
     args = {'-J', url},
   })
@@ -108,7 +141,7 @@ local streamPlaylist = coroutine.wrap(function(url, beginWith)
       connection:playFFmpeg(stream)
     end
   end
-end)
+end)]] -- deprecated
 
 --client.voice:loadOpus('libopus-x86')
 --client.voice:loadSodium('libsodium-x86')
@@ -126,35 +159,59 @@ client:on('messageCreate', function(message)
     if string.find(msg.content, 'audio%.play ') then
       if message.member then
         channel = message.member.voiceChannel
+        guild = message.guild
       end
       if channel then
-        connection = channel:join() -- chances are this is bugged?
+        connections[guild] = channel:join() -- chances are this is bugged on lit discordia. get from github instead.
       end
-      print("channel",channel)
-      print("connection",connection)
-      if connection then
-        print('success!')
+      print(guild,"channel",channel)
+      print(guild,"connection",connection)
+      if connections[guild] then
+        print(guild,'success!')
         playingURL = string.gsub(msg.content, 'audio%.play ', '')
-        local stream = getStream(playingURL) -- URL goes here
-        print("stream",stream)
-        print('playing!')
-        connection:playFFmpeg(stream)
+        local stream,streams = getStream(playingURL) -- URL goes here
+        if stream then
+          print(guild,"stream",stream)
+          print(guild,'playing!')
+          if stream and stream~="multiple" then -- todo: make not shit
+            connections[guild]:playFFmpeg(stream)
+          elseif streams and stream=="multiple" then
+            for i,stream in ipairs(streams) do
+              if true then
+                connections[guild]:playFFmpeg(stream)
+              end
+            end
+          else
+            print(guild,"no streams")
+          end
+        else
+          print(guild,"no stream!")
+        end
       end
-    elseif string.find(msg.content, 'audio%.playlist ') then
+    --[[elseif string.find(msg.content, 'audio%.playlist ') then
       playingURL = string.gsub(msg.content, 'audio%.playlist ', '')
-      streamPlaylist(playingURL, 2)
+      streamPlaylist(playingURL, 2)]] -- deprecated
     elseif msg.content == 'audio.pause' then
-      if connection then
-        connection:pauseStream(playingURL)
+      guild = message.guild
+      if connections[guild] then
+        connections[guild]:pauseStream(playingURL)
       end
     elseif msg.content == 'audio.resume' then
-      if connection then
-        connection:resumeStream()
+      guild = message.guild
+      if connections[guild] then
+        connections[guild]:resumeStream()
       end
     elseif msg.content == 'audio.skip' then
-      print('stopping')
-      if connection then
-        connection:stopStream()
+      guild = message.guild
+      print(guild,'skipping')
+      if connections[guild] then
+        connections[guild]:stopStream()
+      end
+    elseif msg.content == 'audio.leave' then
+      guild = message.guild
+      print(guild,'stopping')
+      if connections[guild] then
+        connections[guild]:close()
       end
     end
   end
